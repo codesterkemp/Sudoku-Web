94065135979639835af5c4858054a975063057b8
codesterkemp <sparky_csk@yahoo.com>
2013-08-02 10:05:20 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SudokuWeb::Application.config.secret_token = '3c771cb41f1754c73f9259b0044e86b1d3b4b4874b9fbc06ea7bb7c8233e59ce2638325f2da944515cf39fa47ef98bf800fff320fb6f9394ee0963652ac6b407'
